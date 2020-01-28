3646618e3a8817b1d9c4d94eb89e72dfb5cc1a9a
hector <Hector.puk@gmail.com>
2020-01-27 23:58:14 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TB1300
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnInvoice = new System.Windows.Forms.Button();
+            this.btnPayment = new System.Windows.Forms.Button();
+            this.btnXml = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(36, 35);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(140, 45);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(376, 35);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(140, 45);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(36, 99);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(140, 30);
+            this.btnOrder.TabIndex = 2;
+            this.btnOrder.Text = "Order";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnInvoice
+            // 
+            this.btnInvoice.Location = new System.Drawing.Point(206, 99);
+            this.btnInvoice.Name = "btnInvoice";
+            this.btnInvoice.Size = new System.Drawing.Size(140, 30);
+            this.btnInvoice.TabIndex = 3;
+            this.btnInvoice.Text = "Invoice";
+            this.btnInvoice.UseVisualStyleBackColor = true;
+            this.btnInvoice.Click += new System.EventHandler(this.btnInvoice_Click);
+            // 
+            // btnPayment
+            // 
+            this.btnPayment.Location = new System.Drawing.Point(376, 99);
+            this.btnPayment.Name = "btnPayment";
+            this.btnPayment.Size = new System.Drawing.Size(140, 30);
+            this.btnPayment.TabIndex = 4;
+            this.btnPayment.Text = "Payment";
+            this.btnPayment.UseVisualStyleBackColor = true;
+            this.btnPayment.Click += new System.EventHandler(this.btnPayment_Click);
+            // 
+            // btnXml
+            // 
+            this.btnXml.Location = new System.Drawing.Point(36, 149);
+            this.btnXml.Name = "btnXml";
+            this.btnXml.Size = new System.Drawing.Size(140, 30);
+            this.btnXml.TabIndex = 5;
+            this.btnXml.Text = "WorkingWithXml";
+            this.btnXml.UseVisualStyleBackColor = true;
+            this.btnXml.Click += new System.EventHandler(this.btnXml_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 201);
+            this.Controls.Add(this.btnXml);
+            this.Controls.Add(this.btnPayment);
+            this.Controls.Add(this.btnInvoice);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "03_DIAPI_Xml";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnInvoice;
+        private System.Windows.Forms.Button btnPayment;
+        private System.Windows.Forms.Button btnXml;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TB1300
+{
+    public partial class Form1 : Form
+    {
+        SAPbobsCOM.Company oCompany;
+        string MySalesOrder;
+        string MySalesInvoice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                oCompany = new SAPbobsCOM.Company();
+                oCompany.Server = "192.168.1.233:30015";
+                oCompany.SLDServer = "192.168.1.233:40000";
+                oCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_HANADB;
+                oCompany.CompanyDB = "SBODEMOES";
+                oCompany.UserName = "manager";
+                oCompany.Password = "Inicio3*";
+                oCompany.DbUserName = "SYSTEM";
+                oCompany.DbPassword = "Kaiser$641";
+
+                int ret = oCompany.Connect();
+                if (ret == 0)
+                    MessageBox.Show("Connection ok");
+                else
+                    MessageBox.Show("Connection failed: " + oCompany.GetLastErrorCode().ToString() + " - " + oCompany.GetLastErrorDescription());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection error: " + ex.Message);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (oCompany.Connected == true)
+                {
+                    oCompany.Disconnect();
+                    MessageBox.Show("You are now disconnected");
+                }
+                else MessageBox.Show("You are not connected to the company.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oCompany);
+                oCompany = null;
+                Application.Exit();
+            }
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            SAPbobsCOM.Documents oSO;
+            oSO = (SAPbobsCOM.Documents)oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oOrders);
+            try
+            {              
+                oSO.CardCode = "C20000";
+                oSO.DocDueDate = DateTime.Today;
+                oSO.Comments = "Krisztian";
+                oSO.Lines.ItemCode = "A00001";
+                oSO.Lines.Quantity = 2;
+                oSO.Lines.Price = 100;
+                oSO.Lines.Add();
+                oSO.Lines.ItemCode = "A00002";
+                oSO.Lines.Quantity = 1;
+                oSO.Lines.Price = 50;
+
+                int ret = oSO.Add();
+
+                if (ret == 0)
+                {
+                    oCompany.GetNewObjectCode(out MySalesOrder);
+                    MessageBox.Show("Add Sales Order successfull - " + MySalesOrder);
+                }
+                else
+                {
+                    MessageBox.Show("Add Sales Order failed: " + oCompany.GetLastErrorDescription());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oSO);
+                oSO = null;
+            }
+        }
+
+        private void btnInvoice_Click(object sender, EventArgs e)
+        {
+            SAPbobsCOM.Documents oSO;
+            SAPbobsCOM.Documents oInv;
+
+            oSO = (SAPbobsCOM.Documents)oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oOrders);
+            oInv = (SAPbobsCOM.Documents)oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oInvoices);
+
+            try
+            {
+                oSO.GetByKey(Int32.Parse(MySalesOrder));
+                oInv.CardCode = oSO.CardCode;
+
+                for (int i = 0; i <= oSO.Lines.Count - 1; i++)
+                {
+                    oInv.Lines.BaseEntry = oSO.DocEntry;
+                    oInv.Lines.BaseLine = i;
+                    oInv.Lines.BaseType = 17;
+                    oInv.Lines.Add();
+                }
+
+                int ret = oInv.Add();
+                if (ret == 0)
+                {
+                    oCompany.GetNewObjectCode(out MySalesInvoice);
+                    MessageBox.Show("Add Invoice successfully - " + MySalesInvoice);
+                }                   
+                else
+                    MessageBox.Show("Add Invoice failed: " + oCompany.GetLastErrorDescription());
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oSO);
+                oSO = null;
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oInv);
+                oInv = null;
+            }
+        }
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+            SAPbobsCOM.Payments oPay;
+            oPay = (SAPbobsCOM.Payments)oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oIncomingPayments);
+            SAPbobsCOM.Documents oInv;
+            oInv = (SAPbobsCOM.Documents)oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oInvoices);
+            try
+            {
+                oInv.GetByKey(Int32.Parse(MySalesInvoice));
+                oPay.CardCode = oInv.CardCode;
+
+                oPay.Invoices.DocEntry = oInv.DocEntry;
+                oPay.CashAccount = "211100";
+                oPay.CashSum = oInv.DocTotal;
+
+
+                int ret = oPay.Add();
+                if (ret == 0)
+                    MessageBox.Show("Add Payment successfully");
+                else
+                    MessageBox.Show("Add Payment failed: " + oCompany.GetLastErrorDescription());
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void btnXml_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                oCompany.XmlExportType = SAPbobsCOM.BoXmlExportTypes.xet_ExportImportMode;
+                SAPbobsCOM.Documents oInv;
+
+                oInv = oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oInvoices);
+
+                oInv.CardCode = "C20000";
+                oInv.DocDueDate = DateTime.Today;
+                oInv.Comments = "invoice saved as xml";
+                oInv.Lines.ItemCode = "A00001";
+                oInv.Lines.Quantity = 2;
+                oInv.Lines.Price = 100;
+                oInv.Lines.Add();
+                oInv.Lines.ItemCode = "A00002";
+                oInv.Lines.Quantity = 1;
+                oInv.Lines.Price = 50;
+
+                oInv.Add();
+                oCompany.GetNewObjectCode(out MySalesInvoice);
+
+                if (oInv.GetByKey(Int32.Parse(MySalesInvoice)) == true)
+                {
+                    oInv.SaveXML("c:\\temp\\invoice_" + MySalesInvoice + ".xml");
+                    MessageBox.Show("Invoice " + MySalesInvoice + " exported to XML");
+                }
+
+                else
+                    MessageBox.Show("Get Invoice failed: " + MySalesInvoice + " - " + oCompany.GetLastErrorDescription());
+
+                string schema;
+                schema = oCompany.GetBusinessObjectXmlSchema(SAPbobsCOM.BoObjectTypes.oInvoices);
+                //MessageBox.Show(schema);
+
+                oInv = oCompany.GetBusinessObjectFromXML("c:\\temp\\invoice_" + MySalesInvoice + ".xml", 0);
+                oInv.Comments = "invoice loaded from xml";
+                int ret = oInv.Add();
+
+                if (ret == 0)
+                {
+                    oCompany.GetNewObjectCode(out MySalesInvoice);
+                    MessageBox.Show("Invoice " + MySalesInvoice + " added from xml.");
+                }
+                    
+                else
+                    MessageBox.Show("Adding invoice " + MySalesInvoice + " from XML failed: " + oCompany.GetLastErrorDescription());
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+    }
+}
